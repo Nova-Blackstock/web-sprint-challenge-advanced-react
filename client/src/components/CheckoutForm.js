dce869cf5e334ce892746d3b0c37ff024061b6c3
Nova Blackstock <fractalrefletions@gmail.com>
2020-08-07 09:59:52 -0700

    Sprint MVP

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { useForm } from '../hooks/useForm'
 
-const initialValue = {
+const initialValues = {
   firstName: "",
   lastName: "",
   address: "",
@@ -15,11 +16,11 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleChanges] = useForm(
+    "checkoutForm",
+    initialValues
+    );
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
